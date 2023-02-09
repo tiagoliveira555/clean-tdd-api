004080e3ca30ed65e0cc4fdb365bb1884b885cb1
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-02-09 19:15:50 -0300

    ensure requests return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,8 @@
 import { Express } from 'express'
-import { bodyParser, cors } from '@/main/middlewares'
+import { bodyParser, contentType, cors } from '@/main/middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
