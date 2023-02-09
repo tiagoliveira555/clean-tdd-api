970c425bf053498b699283da66d05ffbc6c030f7
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-02-09 19:08:11 -0300

    ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,7 @@
 import { Express } from 'express'
-import { bodyParser } from '@/main/middlewares/body-parser'
+import { bodyParser, cors } from '@/main/middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
