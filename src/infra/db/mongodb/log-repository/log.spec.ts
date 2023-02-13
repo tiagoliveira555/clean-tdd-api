006e679348eac5218d11b55016f026cd914fedb0
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-02-12 21:06:14 -0300

    ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,28 @@
+import { LogMongoRepository } from './log'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+
+import { Collection } from 'mongodb'
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    errorCollection = MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('should create an error log on success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
