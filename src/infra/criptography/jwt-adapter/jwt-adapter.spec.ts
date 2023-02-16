3d26ec3ce3c20fa8d6e9daa989a4a914d938c973
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-02-16 20:13:20 -0300

    ensure JwtAdapter thrwos if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -21,4 +21,13 @@ describe('JWT Adapter', () => {
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toEqual('any_token')
   })
+
+  it('should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
+  })
 })
