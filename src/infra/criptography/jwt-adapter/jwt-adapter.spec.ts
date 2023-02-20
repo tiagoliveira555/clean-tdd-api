c335fda9b0a2ce7393e7a644c1bedf401e4389ca
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-02-20 11:01:09 -0300

    ensure JwtAdapter calls verify with correct value

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -5,6 +5,10 @@ import jwt from 'jsonwebtoken'
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return await Promise.resolve('any_token')
+  },
+
+  async verify (): Promise<string> {
+    return await Promise.resolve('any_value')
   }
 }))
 
@@ -37,27 +41,12 @@ describe('JWT Adapter', () => {
     })
   })
 
-  describe('sign()', () => {
-    it('should calls sign with correct values', async () => {
+  describe('verify()', () => {
+    it('should calls verify with correct values', async () => {
       const sut = makeSut()
-      const signSpy = jest.spyOn(jwt, 'sign')
-      await sut.encrypt('any_id')
-      expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
-    })
-
-    it('should return a token on sign success', async () => {
-      const sut = makeSut()
-      const accessToken = await sut.encrypt('any_id')
-      expect(accessToken).toEqual('any_token')
-    })
-
-    it('should throw if sign throws', async () => {
-      const sut = makeSut()
-      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
-        throw new Error()
-      })
-      const promise = sut.encrypt('any_id')
-      await expect(promise).rejects.toThrow()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
   })
 })
--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,12 +1,17 @@
-import { Encrypter } from '@/data/protocols/criptography'
+import { Decrypter, Encrypter } from '@/data/protocols/criptography'
 
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (value: string): Promise<string> {
     const accessToken = jwt.sign({ id: value }, this.secret)
     return await Promise.resolve(accessToken)
+  }
+
+  async decrypt (value: string): Promise<string> {
+    jwt.verify(value, this.secret)
+    return await Promise.resolve(null)
   }
 }
