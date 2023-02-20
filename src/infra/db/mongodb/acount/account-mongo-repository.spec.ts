0a24ac47222ae20ff7e818091731e4a8cd161e01
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-02-20 11:28:47 -0300

    ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/acount/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/acount/account-mongo-repository.spec.ts
@@ -113,10 +113,10 @@ describe('Account Mongo Repository', () => {
       expect(account.password).toBe('any_password')
     })
 
-    // it('should return null if loadByEmail fails', async () => {
-    //   const sut = makeSut()
-    //   const account = await sut.loadByEmail('any_email@mail.com')
-    //   expect(account).toBeFalsy()
-    // })
+    it('should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeFalsy()
+    })
   })
 })
