16e85bc890acf675ba7ec35b885bbe58c97fec55
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-02-20 17:30:36 -0300

    ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -42,6 +42,29 @@ describe('Survey Mongo Repository', () => {
   })
 
   describe('loadAll()', () => {
-
+    it('should loadAll surveys on success', async () => {
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }, {
+        question: 'other_question',
+        answers: [{
+          image: 'other_image',
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }])
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
+    })
   })
 })
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,24 @@
 import { MongoHelper } from '@/infra/db/mongodb/helpers'
-import { AddSurveyRepository } from '@/data/protocols/db/survey'
+import { AddSurveyRepository, LoadSurveysRepository } from '@/data/protocols/db/survey'
 import { AddSurveyModel } from '@/domain/usecases'
+import { SurveyModel } from '@/domain/models'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = MongoHelper.getCollection('surveys')
+    const surveysDataMongo = await surveyCollection.find().toArray()
+    MongoHelper.map(surveysDataMongo)
+    const surveys = surveysDataMongo.map(survey => ({
+      id: survey._id.toHexString(),
+      question: survey.question,
+      answers: survey.answers,
+      date: survey.date
+    }))
+    return surveys
   }
 }
