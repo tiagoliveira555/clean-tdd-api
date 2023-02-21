8e8aa786b4028e98225e708a0d718b76a82e4e5f
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-02-21 10:04:40 -0300

    ensure DbLoadSurveyById returns a survey if LoadSurveyByIdRepository succeeds

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -52,4 +52,10 @@ describe('DbLoadSurveyById', () => {
     await sut.loadById('any_id')
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
   })
+
+  it('should result Survey on success', async () => {
+    const { sut } = makeSut()
+    const survey = await sut.loadById('any_id')
+    expect(survey).toEqual(makeFakeSurvey())
+  })
 })
