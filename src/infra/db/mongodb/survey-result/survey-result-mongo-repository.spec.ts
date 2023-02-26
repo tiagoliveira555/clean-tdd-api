bc4d3828d9742bbc07b072d905da1b83331e48c9
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-02-26 10:29:34 -0300

    ensure SurveyResultMongoRepository returns null on load survey without answers

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -123,5 +123,13 @@ describe('Survey Result Mongo Repository', () => {
       expect(surveyResult.answers[2].count).toBe(0)
       expect(surveyResult.answers[2].percent).toBe(0)
     })
+
+    it('should return null if there is no survey result', async () => {
+      const survey = await makeSurvey()
+      const accountId = await makeAccountId()
+      const sut = makeSut()
+      const surveyResult = await sut.loadBySurveyId(survey.id, accountId)
+      expect(surveyResult).toBeNull()
+    })
   })
 })
